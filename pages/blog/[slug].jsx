5e7b25bc59ee91a6848a5ac8df6d504326316855
ladjeek-actions <admin@hesiy.cn>
2023-02-01 19:37:10 +0800

    无破坏性修改

--- pages/blog/[slug].jsx
+++ pages/blog/[slug].jsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Script from 'next/Script'
+import Script from 'next/Script';
 /* import dynamic from 'next/dynamic'; */
 import Image from 'next/image';
 import { MDXRemote } from 'next-mdx-remote';
